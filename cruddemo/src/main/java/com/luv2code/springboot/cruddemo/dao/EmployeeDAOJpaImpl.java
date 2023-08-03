5175209e0697c6ff01dd7b0f1a39677e51536b63
Eduardo <40169421+skullino@users.noreply.github.com>
2023-08-03 13:24:09 -0300

    Created EmployeeDAO;

---
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,23 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+    private EntityManager entityManager;
+
+    public EmployeeDAOJpaImpl(EntityManager theEntityManager) {
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+        TypedQuery<Employee> theQuery = entityManager.createQuery("FROM Employee", Employee.class);
+        return theQuery.getResultList();
+    }
+}
---
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/entity/Employee.java
@@ -0,0 +1,71 @@
+package com.luv2code.springboot.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+    @Id
+    @GeneratedValue
+    @Column(name="id")
+    private int id;
+
+    @Column(name="firstName")
+    private String firstName;
+
+    @Column(name="lastName")
+    private String lastname;
+
+    @Column(name="email")
+    private String email;
+
+    public Employee() {}
+
+    public Employee(String firstName, String lastname, String email) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
